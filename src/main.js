1c3468cb8dd3102c3359ca44ff72756f2ebb9350
Tuananh4403 <tuananh4403@gmail.com>
2025-02-21 20:40:44 +0700

    update resource

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
-import './assets/main.css'
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import 'bootstrap';
 import { createApp } from 'vue'
 import App from '../src/pages/HomePage.vue'
 import store from '@/store/store';
 import router from '@/router/router';
-import './assets/multivue.css';
-import './assets/style.css';
-// import 'toastr/build/toastr.min.css';
+import mitt from 'mitt';
+import Toast, { useToast } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+async function bootstrap() {
+    const app = createApp(App)
 
-const app = createApp(App)
+    app.use(router);
+    app.use(store)
 
-app.use(router);
-app.use(store)
+    app.mount('#app')
+}
+bootstrap();
 
-app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,74 @@
-import { fileURLToPath, URL } from 'node:url'
+import 'dotenv/config';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { glob } from 'glob';
+import path from 'node:path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+const resourceJsFiles = glob.sync('src/**/*.js');
+const resourceCssFiles = glob.sync('src/**/*.css');
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    server: {
+        port: process.env.VITE_SERVER_PORT || 5173,
+        host: '0.0.0.0',
     },
-  },
-  server: {
-    port: 3000 
-  }
-})
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'public/assets/css/style.css',
+                    dest: './'
+                },
+            ]
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "~scss": path.resolve(__dirname, "./public/assets/scss"),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~scss/abstracts/variables" as *;`,
+                api: 'modern-compiler'
+            },
+        },
+    },
+    build: {
+        target: 'esnext',
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (/\.(css)$/.test(assetInfo.name)) {
+                        return 'assets/css/[name]-[hash][extname]';
+                    }
+                    if (/\.(js)$/.test(assetInfo.name)) {
+                        return 'assets/js/[name]-[hash][extname]';
+                    }
+                    if (/\.(png|jpe?g|gif|svg)$/.test(assetInfo.name)) {
+                        return 'assets/images/[name]-[hash][extname]';
+                    }
+                    if (/\.(woff|woff2|ttf|otf)$/.test(assetInfo.name)) {
+                        return 'assets/fonts/[name]-[hash][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
+                manualChunks: {
+                    vue: [
+                        'vue',
+                        'vue-router',
+                        'vue-i18n',
+                        'vue-toastification',
+                        'vue-multiselect'
+                    ],
+                    vendor: ['axios'],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 2000,
+    },
+});
