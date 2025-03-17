db3100c46fbb6affb4de75c1bd92ca563e11664c
Tuananh4403 <tuananh4403@gmail.com>
2025-03-17 16:15:19 +0700

    update booking detail

---
+++ src/components/booking-history/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="side-bar-search">
+          <SidebarSearch @search-clicked="handleSearchClicked" />
+        </div>
+        <div class="side-bar-detail">
+          <SidebarDetail />
+        </div>
+      </div>
+      <div class="col-md-9">
+        <RoomList />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SidebarSearch from "./SidebarSearch.vue";
+import SidebarDetail from "./SidebarDetail.vue";
+import RoomList from "./ListRoom.vue";
+
+export default {
+  components: {
+    SidebarSearch,
+    SidebarDetail,
+    RoomList,
+  },
+  data() {
+    return {
+      showSidebarDetail: false,
+    };
+  },
+  methods: {
+    handleSearchClicked() {
+      this.showSidebarDetail = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin-top: 20px;
+}
+
+.col-md-3 {
+  border-right: 1px solid #c2bebe;
+}
+
+.side-bar-search,
+.side-bar-detail {
+  margin-top: 6%;
+  margin-right: 12%;
+}
+
+.side-bar-detail {
+  margin-bottom: 6%;
+}
+</style>
